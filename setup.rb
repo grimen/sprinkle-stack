7161a6fce4698b5528517db68ad5af0a0eb2e3ab
Sina Iman <sina.iman@gmail.com>
2012-01-03 08:59:44 -0500

    the script now pays attention to roles :app being defined. it will look for the first server defined to the :app role and use that as default. additionally, interactive prompt had a bug and wasn't getting correct values from gets, this is fixed.

--- setup.rb
+++ setup.rb
@@ -48,7 +48,7 @@ deployment do
     default_run_options[:pty] = true
     
     vars = {
-      :app      => {:label => ":app, host domain/IP",     :default => nil},
+      :app      => {:label => ":app, host domain/IP",     :default => (roles[:app].blank?) ? nil : roles[:app].servers.first },
       :deployer => {:label => ":deployer, deploy-user",   :default => 'deployer'},
       :user     => {:label => ":root, setup-user",        :default => 'root'},
       :group    => {:label => ":group, deployers-group",  :default => 'deployer'}
@@ -56,9 +56,9 @@ deployment do
     
     # Ensure defined - if not, then ask.
     vars.keys.each do |var|
-      unless defined?(var) || (value = send(var)).present?
-        print "#{vars[var][:label]} (default: #{vars[var][:default]}): "
-        value = gets || vars[var][:label]
+      unless respond_to_with_variables?(var) && (value = send(var)).present?
+        print "#{vars[var][:label]} [#{vars[var][:default]}]: "
+        value = vars[var][:default] if gets.blank?
         set var, value
       end
     end
@@ -68,7 +68,11 @@ deployment do
       exit
     end
     
-    puts %{[IMPORTANT:] Amazon EC2 images by default don't allow logins as root, therefore login as user "ubuntu" (for Ubuntu, see docs for other distributions). To enable root login: (1) Login as default user, (2) switch to root, (3) remove the leading command-part in '/root/.ssh/authorized_keys' using an editor, (4) Ensure root password is set. Now it should work!}
+    puts %{[IMPORTANT:] Amazon EC2 images by default don't allow logins as root, therefore login as user "ubuntu" (for Ubuntu, see docs for other distributions).\nTo enable root login:\t
+      \t(1) Login as default user
+      \t(2) switch to root
+      \t(3) remove the leading command-part in '/root/.ssh/authorized_keys' using an editor
+      \t(4) Ensure root password is set. Now it should work!}
   end
 
   # Source based package installer defaults.
